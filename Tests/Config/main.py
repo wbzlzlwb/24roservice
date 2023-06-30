382f2a9d0545bcad9517f168dc9366bb42fae3f1
wbzlzlwb <wbzlzlwb@naver.com>
2023-06-30 15:24:49 +0900

    dd

--- Tests/Config/main.py
+++ Tests/Config/main.py
@@ -1,10 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 options = webdriver.ChromeOptions()
